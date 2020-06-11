4873edab2d2025b685db08b502117e69963b6d69
Matheus Pimentel <matheus08.pimentel@gmail.com>
2020-06-11 19:17:39 -0300

    Create the 'routes' post route

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,47 @@
 import express from 'express';
+import knex from './database/connection';
+import { KnexTimeoutError } from 'knex';
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-  return response.json({ message: 'Hello World'});
+routes.get('/items', async (request, response) => {
+  const items = await knex('items').select('*');
+
+  const serializedItems = items.map(item => {
+    return {
+      id: item.id,
+      name: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`,
+    };
+  });
+
+  return response.json(serializedItems);
+});
+
+routes.post('/points', async (request, response) => {
+  const { 
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items
+   } = request.body;
+
+  await knex('points').insert({
+    image: 'image-fake',
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+   });
+
+   return response.json({ sucess: true });
 });
 
 export default routes;
